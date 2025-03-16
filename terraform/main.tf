65af4477336bd2ce0d6822d2166655ee3afa282a
Jim <erenne@hotmail.fr>
2025-03-16 17:51:13 +0000

    looking for a secure way to get secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project = "cloud-1"
+  project = var.gcp_project_id
   region  = "europe-west1"
 }
 
@@ -10,24 +10,69 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-2004-focal-v20220912"
+      image = "ubuntu-2004-focal-v20250313"
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-    }
+    access_config {}
   }
 
   metadata = {
     ssh-keys = "devuser:${file("~/.ssh/id_rsa.pub")}"
   }
+
+  # Transfert du fichier docker-compose.yml sur la VM
+  provisioner "file" {
+    source      = "../docker-compose.yml"
+    destination = "/home/devuser/docker-compose.yml"
+    connection {
+      type        = "ssh"
+      user        = "devuser"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+
+  # Transfert du playbook Ansible sur la VM
+  provisioner "file" {
+    source      = "../ansible/playbook.yml"
+    destination = "/home/devuser/playbook.yml"
+    connection {
+      type        = "ssh"
+      user        = "devuser"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+
+  # Installation des outils nécessaires via apt
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y software-properties-common",
+      "sudo apt install -y python3 python3-pip ansible docker.io docker-compose"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "devuser"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
+
+  # Exécution du playbook Ansible pour démarrer les containers
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-playbook /home/devuser/playbook.yml"
+    ]
+    connection {
+      type        = "ssh"
+      user        = "devuser"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.network_interface[0].access_config[0].nat_ip
+    }
+  }
 }
 
-resource "google_compute_disk" "persistent_disk" {
-  name  = "wordpress-disk"
-  type  = "pd-standard"
-  zone  = "europe-west1-b"
-  size  = 50
-}
